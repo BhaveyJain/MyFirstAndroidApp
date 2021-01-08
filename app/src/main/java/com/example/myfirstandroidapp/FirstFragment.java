3c3ca0bdea33c163043e832ac451e1aecf69d13a
BhaveyJain <jain385@purdue.edu>
2021-01-08 18:02:40 -0500

    set up the path for the first frame to send an Integer to the second frame

--- app/src/main/java/com/example/myfirstandroidapp/FirstFragment.java
+++ app/src/main/java/com/example/myfirstandroidapp/FirstFragment.java
@@ -28,13 +28,6 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
         view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,10 +41,14 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 countMe(view);
             }
+        });
 
-                                                                 }
-
-        );
+        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                randomNumber(view);
+            }
+        });
     }
 
     private void countMe(View view) {
@@ -59,5 +56,10 @@ public class FirstFragment extends Fragment {
         int value = Integer.parseInt(countString);
         value++;
         showCountTextView.setText(String.valueOf(value));
+    }
+
+    private void randomNumber(View view) {
+        int randomNum = (int) (Math.random() * Integer.parseInt(showCountTextView.getText().toString()) + 1);
+        showCountTextView.setText(String.valueOf(randomNum));
     }
 }
